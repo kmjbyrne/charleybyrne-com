039096b585edeca4482d193e4fa54afcfc2eb6e5
Keith Byrne <keithmbyrne@gmail.com>
2022-05-12 01:46:15 +0100

    CONFIG: add package configs

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: ["stylelint-scss"],
+    extends: ["stylelint-config-standard", "stylelint-config-prettier", "stylelint-config-standard-vue", "stylelint-scss"],
+    // add your custom config here
+    // https://stylelint.io/user-guide/configuration
+    rules: {},
+};
